cf749db9245b80d176a5e1b24a57b645e44bc714
Alessio Piccioni <alessio.piccioni@students.uniroma2.eu>
2024-01-16 19:24:32 +0100

    ok

---
+++ app/src/main/java/com/piccionialessio/nine/model/Game.kt
@@ -0,0 +1,13 @@
+package com.piccionialessio.nine.model
+
+import java.time.LocalDate
+import java.util.Date
+
+class Game {
+    var permutation = Array<Int>(9, { it -> 0 })
+    val date: LocalDate = LocalDate.now()
+    /*tempo
+    esito
+    nome
+     */
+}
